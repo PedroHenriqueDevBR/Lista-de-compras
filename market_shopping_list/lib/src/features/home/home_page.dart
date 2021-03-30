64d7ec655fc3bfb8e1d0ef74a674d7199a0d622f
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-03-30 13:09:26 -0300

    style: create homepage

--- market_shopping_list/lib/src/features/home/home_page.dart
+++ market_shopping_list/lib/src/features/home/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:market_shopping_list/src/features/home/home_controller.dart';
 import 'package:market_shopping_list/src/shared/models/family.dart';
+import 'package:market_shopping_list/src/shared/utils/colors_util.dart';
 import 'package:market_shopping_list/src/shared/utils/images_reference_util.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   HomeController _controller = HomeController();
   ImageReference _imageReference = ImageReference();
+  ColorUtil _color = ColorUtil();
 
   @override
   Widget build(BuildContext context) {
@@ -25,61 +27,7 @@ class _HomePageState extends State<HomePage> {
         padding: EdgeInsets.all(8),
         physics: BouncingScrollPhysics(),
         itemBuilder: (context, index) {
-          Family family = _controller.families[index];
-          return Card(
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  alignment: Alignment.center,
-                  fit: BoxFit.cover,
-                  image: NetworkImage('https://images.pexels.com/photos/1724888/pexels-photo-1724888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
-                ),
-              ),
-              height: 130,
-              child: ClipRRect(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          family.name,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset.zero,
-                                blurRadius: 4.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Text(
-                          'Grupo pertencente a ${family.name}',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 14,
-                            shadows: <Shadow>[
-                              Shadow(
-                                offset: Offset(0, 0),
-                                blurRadius: 4.0,
-                                color: Colors.black,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
+          return _familyCardItem(_controller.families[index]);
         },
       ),
       drawer: Drawer(
@@ -88,40 +36,173 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      _imageReference.logo,
-                      width: 40,
-                    ),
-                    SizedBox(width: 16),
-                    Text('Lista de compras'),
-                  ],
-                ),
-              ),
+              _drawerHead(),
               Divider(),
-              TextButton.icon(
-                icon: Icon(Icons.home),
-                label: Text('Familias'),
-                onPressed: () {},
-              ),
-              TextButton.icon(
-                icon: Icon(Icons.calendar_today),
-                label: Text('Compras pendentes'),
-                onPressed: () {},
-              ),
-              TextButton.icon(
-                icon: Icon(Icons.settings),
-                label: Text('Configurações'),
-                onPressed: () {},
-              ),
+              _drawerItemConfigure(icon: Icons.home, text: 'Familias', action: () {}),
+              _drawerItemConfigure(icon: Icons.calendar_today, text: 'Pendentes', action: () {}),
+              _drawerItemConfigure(icon: Icons.settings, text: 'Configurações', action: () {}),
+              _drawerItemConfigure(icon: Icons.exit_to_app_outlined, text: 'Encerrar sessão', action: () {}),
               Divider(),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                child: Text('Famílias'),
+              ),
+              ..._getFamilieListForDrawer(),
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: _color.secondaryColor,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8.0),
+                topRight: Radius.circular(8.0),
+              ),
+            ),
+            builder: (context) => Wrap(
+              children: [
+                Column(
+                  children: [
+                    ListTile(
+                      title: Text('Criar família'),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: Text('Entrar em uma família'),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
+  }
+
+  Widget _familyCardItem(Family family) {
+    return Card(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            alignment: Alignment.center,
+            fit: BoxFit.cover,
+            image: NetworkImage('https://images.pexels.com/photos/1724888/pexels-photo-1724888.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+          ),
+        ),
+        height: 130,
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    family.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset.zero,
+                          blurRadius: 4.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    'Grupo pertencente a ${family.name}',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset(0, 0),
+                          blurRadius: 4.0,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _drawerHead() {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Row(
+        children: [
+          Image.asset(
+            _imageReference.logo,
+            width: 40,
+          ),
+          SizedBox(width: 16),
+          Text('Lista de compras'),
+        ],
+      ),
+    );
+  }
+
+  Widget _drawerItemConfigure({
+    required IconData icon,
+    required String text,
+    required Function action,
+  }) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(text),
+      onTap: () {
+        action();
+      },
+    );
+  }
+
+  Widget _drawerFamilyItem({
+    required Family family,
+  }) {
+    return ListTile(
+      leading: CircleAvatar(
+        child: Text(
+          _getFamilyNameFirstLetters(family.name),
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: _color.secondaryColor,
+      ),
+      title: Text(family.name),
+    );
+  }
+
+  List<Widget> _getFamilieListForDrawer() {
+    List<Widget> familieList = [];
+    for (Family family in _controller.families) {
+      familieList.add(_drawerFamilyItem(family: family));
+    }
+    return familieList;
+  }
+
+  String _getFamilyNameFirstLetters(String familyName) {
+    List<String> splitName = familyName.split(' ');
+    if (splitName.length > 1) {
+      return '${splitName[0][0]}${splitName[1][0]}';
+    }
+    return '${splitName[0][0]}';
   }
 }
--- market_shopping_list/lib/src/shared/utils/colors_util.dart
+++ market_shopping_list/lib/src/shared/utils/colors_util.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ColorUtil {
-  Color bgColor = Color(0XFFEDF6F9);
-  Color primaryColor = Color(0XFF006D77);
-  Color primaryColorLight = Color(0XFF83C5BE);
-  Color secondaryColor = Color(0XFFE29578);
-  Color secondaryColorLight = Color(0XFFFFDDD2);
+  Color bgColor = Color(0XFFEFF6E0);
+  Color primaryColor = Color(0XFF598392);
+  Color primaryColorLight = Color(0XFFAEC3B0);
+  Color secondaryColor = Color(0XFFDB504A);
+  Color secondaryColorDark = Color(0XFF102B3F);
 
   Color gmailColor = Color(0XFFC71610);
 }
